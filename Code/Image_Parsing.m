b0b2084aa9bf420c1b6f2f6d4b3c145a97a33e12
erinkidd01 <ecoughlan@hmc.edu>
2012-11-16 15:47:58 -0800

    gets all file names from current directory and parses based on emotion

--- Code/Image_Parsing.m
+++ Code/Image_Parsing.m
@@ -1,9 +1,52 @@
 % Image Parsing
 % Erin Coughlan
 
-string = 'name_dir_emotion_open.pgm';
-% split the filenames to get emotions and to compare
-% example:
-% string = 'name_dir_emotion_open.pgm';
-% arr = ['name' 'dir' 'emotion' 'open' 'pgm'];
-arr = regexp(string, '_|\.', 'split');
+% set constants for array index
+NAME = 1;
+DIR = 2;
+EMOTION = 3;
+OPEN = 4;
+EXT = 5;
+
+% set constants for emtions
+ANGRY = 1;
+HAPPY = 2;
+NEUTRAL = 3;
+SAD = 4;
+
+dir = strcat(pwd, '/all_images');
+fileList = getAllFiles(dir);
+
+total = 0;
+[nFiles,nIn]=size(fileList);
+for i = 1:nFiles
+    fileName = fileList{i};
+    % split the filenames to get emotions and to compare
+    % example:
+    % string = 'name_dir_emotion_open.pgm';
+    % arr = ['name' 'dir' 'emotion' 'open' 'pgm'];
+    arr = regexp(fileName, '_|\.', 'split');
+
+    % initialize hot code
+    hotCode = [0 0 0 0];
+    arrEmo = arr(EMOTION);
+    if strcmp('angry', arrEmo)
+        hotCode(ANGRY) = 1;
+        total = total + 1;
+    elseif strcmp('happy', arrEmo)
+        hotCode(HAPPY) = 1;
+        total = total + 1;
+    elseif strcmp('neutral', arrEmo)
+        hotCode(NEUTRAL) = 1;
+        total = total + 1;
+    elseif strcmp('sad', arrEmo)
+        hotCode(SAD) = 1;
+        total = total + 1;
+    else
+        disp('No emotion data');
+    end
+end
+
+total
+
+        
