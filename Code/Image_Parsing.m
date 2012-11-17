fac1bc95d6169dd11b9ec7e7bd4cf2b8403d838c
erinkidd01 <ecoughlan@hmc.edu>
2012-11-16 16:11:02 -0800

    Parsing on direction and glasses completed

--- Code/Image_Parsing.m
+++ Code/Image_Parsing.m
@@ -18,32 +18,45 @@ dir = strcat(pwd, '/all_images');
 fileList = getAllFiles(dir);
 
 total = 0;
+finalFileList = cell(0);
 [nFiles,nIn]=size(fileList);
 for i = 1:nFiles
     fileName = fileList{i};
+    
     % split the filenames to get emotions and to compare
     % example:
     % string = 'name_dir_emotion_open.pgm';
     % arr = ['name' 'dir' 'emotion' 'open' 'pgm'];
     arr = regexp(fileName, '_|\.', 'split');
-
-    % initialize hot code
-    hotCode = [0 0 0 0];
+    
+    arrDir = arr(DIR);
     arrEmo = arr(EMOTION);
-    if strcmp('angry', arrEmo)
-        hotCode(ANGRY) = 1;
+    arrOpen = arr(OPEN);
+    
+    success = false;
+    % We only want to look at pictures that are straight on
+    % and don't have glasses
+    if strcmp('straight', arrDir) && strcmp('open', arrOpen)
+        % initialize hot code
+        hotCode = [0 0 0 0];
+        success = true;
+        if strcmp('angry', arrEmo)
+            hotCode(ANGRY) = 1;
+        elseif strcmp('happy', arrEmo)
+            hotCode(HAPPY) = 1;
+        elseif strcmp('neutral', arrEmo)
+            hotCode(NEUTRAL) = 1;
+        elseif strcmp('sad', arrEmo)
+            hotCode(SAD) = 1;
+        else
+            disp('No emotion data');
+            success = false;
+        end
+    end
+    
+    if success
+        finalFileList = [finalFileList; fileName];
         total = total + 1;
-    elseif strcmp('happy', arrEmo)
-        hotCode(HAPPY) = 1;
-        total = total + 1;
-    elseif strcmp('neutral', arrEmo)
-        hotCode(NEUTRAL) = 1;
-        total = total + 1;
-    elseif strcmp('sad', arrEmo)
-        hotCode(SAD) = 1;
-        total = total + 1;
-    else
-        disp('No emotion data');
     end
 end
 
