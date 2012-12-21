2f8c01a93c9fbf6507d013603721a8a63cfb8bb6
erinkidd01 <ecoughlan@hmc.edu>
2012-12-21 08:38:24 -0800

    Added the working canny file

--- Code/Image_Parsing.py
+++ Code/Image_Parsing.py
@@ -4,7 +4,6 @@
 
 # Image Parsing
 
-#from getAllFiles import *
 from constants import *
 
 import os, sys
@@ -13,8 +12,6 @@ import re
 import cv
 import numpy as np
 import scipy
-import matplotlib.pyplot as plt
-
 
 
 def getAllFiles(ext):
@@ -26,11 +23,10 @@ def getAllFiles(ext):
     Return type: String(currDir), [String(filenames)]
     """
 
-    currDir = os.getcwd();
-    currDir += ext;
-    filenames = os.listdir(currDir);
-    filenames.pop(0) # to get rid of the github hidden file
-    return currDir, filenames;
+    currDir = os.getcwd()
+    currDir += ext
+    filenames = os.listdir(currDir)
+    return currDir, filenames
 
 
 
@@ -43,53 +39,62 @@ def hot(fileList):
     Return type: fileList
     """
 
-    total = 0;
-    finalFileList = [];
-    nFiles = len(fileList);
+    total = 0
+    finalFileList = []
+    nFiles = len(fileList)
     for i in range(nFiles):
-        filename = fileList[i];
+        filename = fileList[i]
         
         # split filenames to get components
-        arr = re.split(r"[_.]",filename);
+        arr = re.split(r"[_.0]",filename)
 
-        arrDir = arr[DIR];
-        arrEmo = arr[EMOTION];
-        arrOpen = arr[OPEN];
-        
-        success = False;
-        # We only want to look at pictures that are straight on
-        # and don't have glasses
-        if 'straight' == arrDir and 'open' == arrOpen:
-            # initialize hot code
-            hotCode = [0,0,0,0];
-            success = True;
-            if 'angry' == arrEmo:
-                hotCode[ANGRY] = 1;
-            elif 'happy' == arrEmo:
-                hotCode[HAPPY] = 1;
-            elif 'neutral' == arrEmo:
-                hotCode[NEUTRAL] = 1;
-            elif 'sad' == arrEmo:
-                hotCode[SAD] = 1;
-            else:
-                print 'No emotion data';
-                success = False;
+        success = False
+        if arr[0] != '': # ignoring hidden files
+            arrDir = arr[DIR]
+            arrEmo = arr[EMOTION]
+            arrOpen = arr[OPEN]
+            
+            # We only want to look at pictures that are straight on
+            # and don't have glasses
+            if 'straight' == arrDir and 'open' == arrOpen:
+                # initialize hot code
+                hotCode = [0,0,0,0]
+                success = True;
+                if 'angry' == arrEmo:
+                    hotCode[ANGRY] = 1
+                elif 'happy' == arrEmo:
+                    hotCode[HAPPY] = 1
+                elif 'neutral' == arrEmo:
+                    hotCode[NEUTRAL] = 1
+                elif 'sad' == arrEmo:
+                    hotCode[SAD] = 1
+                else:
+                    print 'No emotion data'
+                    success = False
         
         if success:
-            finalFileList.append([filename, hotCode]);
-            total = total + 1;
+            finalFileList.append([filename, hotCode])
+            total = total + 1
 
-    print "total: ", total;
-    print "first file and code: ", finalFileList[0]
+    if total != 0:
+        print "total: ", total
+        print "first file and code: ", finalFileList[0]
 
     return finalFileList
 
+
 def read_xml(filename):
-    """ Return image data from an xml file as a numpy array. """
+    """ Return image data from an xml file as an array.
+
+        Input type: String represnting filename
+        Return type: array of data values
+    """
+
     f = open(filename, "r")
     a = []
     foundData = False
-    
+
+    count = 0
     while True:
         line = f.readline()
         if not line:
@@ -97,61 +102,48 @@ def read_xml(filename):
         elif "data" in line:
             foundData = True
         elif foundData:
+            count += 1
             a.append(line.rstrip())
         elif ("data" in line) and foundData:
             line.replace("</data></name>", "")
             a.append(line.rstrip())
             foundData = False
 
-    print a
-    return a
+    f.close()
+
+    ans = []
+    for i in range(len(a)):
+        arr = a[0].split(" ")
+        for j in range(len(arr)):
+            char = arr[j]
+            if char != '':
+                ans.append(int(char))
+                
+    return ans
 
 
 if __name__=="__main__":
 
-  #  currDir, fileList = getAllFiles("/all_images");
-
+ #   currDir, fileList = getAllFiles("/all_images")
  #   finalFileList = hot(fileList)
 
-     finalFileList = [['example.xml', [0,0,1,0]]
+    # Get all the file names and parse for important info
+    currDir, fileList = getAllFiles("/xml")
+    finalFileList = hot(fileList)
 
     num_samples = len(finalFileList)
 
+    ans = []
     for filename, hotCode in finalFileList:
-        fileArr = read_xml(filename)
-        hotArr = find(hotCode, 1)
-        arr = np.append(fileArr, hotArr)
+        fileArr = read_xml(currDir + '/' + filename)
+        hotArr = [hotCode.index(1)]
+        ans.append(fileArr + hotArr)
 
-    # useful for combining numpy arrays -- vstack(tup)
-
-##    arr = []
-##    for sample in range(num_samples):
-##        path = currDir + '/' + finalFileList[sample][0];
-##        hotCode = finalFileList[sample][1]
-##        
-##        face = cv.LoadImageM(path, cv.CV_LOAD_IMAGE_GRAYSCALE);
-##        a = np.asarray( face[:,:] )
-##        nRow, nCol = a.shape
-##        curr = []
-##        for row in range(nRow):
-##            for val in a[row]:
-##                curr.append(val)
-##
-##        arr.append(curr)
-
-##    # experiement on the first image
-##    a = np.array(arr[0])
-##    size = a.size
-##    np.savetxt('first_face.txt', a)
-##
-##    # make the image come back
-##    new_data = np.loadtxt('first_face.txt')
-##    new_data = new_data.reshape((120,128))
-##
-##    plt.imshow(new_data) #Needs to be in row,col order
-##    plt.savefig('haha.png')
+    final = np.array(ans)
+    np.savetxt('data.txt', final, fmt='%10.f')
  
 
+    # Sample data sets for testing
     # Save data to a text file using numpy
     # classes as target
     x1 = [[1,0,0,0,0,2],[0,0,0,1,0,2],[1,0,0,0,0,1],[1,1,1,0,0,0],
@@ -168,12 +160,7 @@ if __name__=="__main__":
          [1,0,0,0,1,0,0,0,1],[0,0,0,1,0,0,0,1,0],[1,0,0,0,0,0,1,0,0],[1,1,1,0,0,0,0,0,0]]
     
     x = np.array(x1)
-    print x
- #   x = np.arange(20).reshape((4,5))
+    
     # fmt determine format numbers should be in
-    np.savetxt('test.txt', x, fmt='%10.5f')
-##
-##    # To read info back using numpy
-##    new_data = np.loadtxt('test.txt')
-##    new_data = new_data.reshape((4,5))
+    np.savetxt('test.txt', x, fmt='%10.f')
 
